8719b1dea974daaf36311c7bc8f24100e31b4a6a
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2022-08-16 01:07:40 -0300

    feat: adding redirect and link to edit a product

--- src/admin/Products.tsx
+++ src/admin/Products.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Product } from "../interfaces/product";
+import { Link } from "react-router-dom";
 
 import Wrapper from "./Wrapper";
+import { Product } from "../interfaces/product";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -16,17 +17,28 @@ const Products = () => {
   }, []);
 
   const productDelete = async (id: number) => {
-    if (window.confirm('Are you sure you want to delete this product?')){
+    if (window.confirm("Are you sure you want to delete this product?")) {
       await fetch(`http://localhost:8000/api/products/${id}`, {
-        method: 'DELETE'
-      })
-  
-      setProducts(products.filter((p: Product) => p.id !== id))
+        method: "DELETE",
+      });
+
+      setProducts(products.filter((p: Product) => p.id !== id));
     }
-  }
+  };
 
   return (
     <Wrapper>
+      <div className="pt-3 pb-2 mb-3 border-bottom">
+        <div className="btn-toolbar mb-2 mb-md-0">
+          <Link
+            to="/admin/products/create"
+            className="btn btn-sm btn-outline-secondary"
+          >
+            Add
+          </Link>
+        </div>
+      </div>
+
       <div className="table-responsive">
         <table className="table table-striped table-sm">
           <thead>
@@ -43,12 +55,26 @@ const Products = () => {
               return (
                 <tr key={p.id}>
                   <td>{p.id}</td>
-                  <td><img src={p.image} height="50" /></td>
+                  <td>
+                    <img src={p.image} height="50" />
+                  </td>
                   <td>{p.title}</td>
                   <td>{p.likes}</td>
                   <td>
-                    <div>
-                      <a href="#" className="btn btn-sm btn-outline-secondary" onClick={() => productDelete(p.id)}>Delete</a>
+                    <div className="btn-group mr-2">
+                      <Link
+                        to={`/admin/products/${p.id}/edit`}
+                        className="btn btn-sm btn-outline-primary"
+                      >
+                        Edit
+                      </Link>
+                      <a
+                        href="#"
+                        className="btn btn-sm btn-outline-danger"
+                        onClick={() => productDelete(p.id)}
+                      >
+                        Delete
+                      </a>
                     </div>
                   </td>
                 </tr>
